9214cb3ca2d9cf5f192bf7ff164eef79421e9fbc
HamzaA598 <hamzaemad598@gmail.com>
2023-11-14 13:44:13 +0200

    add: template

---
+++ model.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import scikitlearn
+
+
+# Data Analysis
+
+
+# Preprocessing
+
+
+# Linear Regression
+
+
+# Logistic Regression
+
+
+# Accuracy
+
+
+# Training
+
+
+# Predict
